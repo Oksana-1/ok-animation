e46d6cff784b257f636838778a2a607b63a4b9fc
oksana-1 <4438488@gmail.com>
2022-01-09 14:36:34 +0200

    Small refactoring

--- src/Target.js
+++ src/Target.js
@@ -1,36 +1,35 @@
 import { HeightAnimation } from "./Animations.js";
 
 export const Target = {
-	targetElement: null,
-	isActive() {
-		if (!this.targetElement) return false;
-		const stateAttr = this.targetElement.getAttribute("data-state");
-		return Boolean(stateAttr && stateAttr === "active");
-	},
-	animate() {
-		this.isActive() ? this.animateBackward() : this.animateForward();
-	},
-	animateForward() {
-		const heightAnimation = Object.create(HeightAnimation);
-		heightAnimation.init(this.targetElement);
-		return new Promise((resolve) => {
-			heightAnimation.slideDown().then(() => {
-				this.targetElement.setAttribute("data-state", "active");
-				resolve();
-			});
-		});
-	},
-	animateBackward() {
-		const heightAnimation = Object.create(HeightAnimation);
-		heightAnimation.init(this.targetElement);
-		return new Promise((resolve) => {
-			heightAnimation.slideUp().then(() => {
-				this.targetElement.removeAttribute("data-state");
-				resolve();
-			});
-		});
-	},
-	init(targetElement) {
-		this.targetElement = targetElement;
-	}
-}
+  targetElement: null,
+  heightAnimation: null,
+  isActive() {
+    if (!this.targetElement) return false;
+    const stateAttr = this.targetElement.getAttribute("data-state");
+    return Boolean(stateAttr && stateAttr === "active");
+  },
+  animate() {
+    this.isActive() ? this.animateBackward() : this.animateForward();
+  },
+  animateForward() {
+    return new Promise((resolve) => {
+      this.heightAnimation.slideDown().then(() => {
+        this.targetElement.setAttribute("data-state", "active");
+        resolve();
+      });
+    });
+  },
+  animateBackward() {
+    return new Promise((resolve) => {
+      this.heightAnimation.slideUp().then(() => {
+        this.targetElement.removeAttribute("data-state");
+        resolve();
+      });
+    });
+  },
+  init(targetElement) {
+    this.targetElement = targetElement;
+    this.heightAnimation = Object.create(HeightAnimation);
+    this.heightAnimation.init(this.targetElement);
+  },
+};
--- src/Trigger.js
+++ src/Trigger.js
@@ -2,21 +2,37 @@ import { Target } from "./Target.js";
 
 export const Trigger = {
   triggerElement: null,
+  targetElement: null,
   animate() {
     this.triggerElement.classList.toggle("active");
   },
   init(triggerElement) {
     this.triggerElement = triggerElement;
+    this.defineTargetElement();
   },
   listen(event) {
-    document.addEventListener(event, async (e) => {
-      const animationId = e.target.getAttribute("data-animate-trigger");
-      if (!animationId) throw new Error("Animation trigger need unique `data-animate-trigger` attribute!");
-      const targetElement = document.querySelector(`[data-animate-target="${animationId}"]`);
+    document.addEventListener(event, async () => {
       const target = Object.create(Target);
-      target.init(targetElement);
+      target.init(this.targetElement);
       await target.animate();
       this.animate();
-    })
+    });
+  },
+  defineTargetElement() {
+    const animationId = this.triggerElement.getAttribute(
+      "data-animate-trigger"
+    );
+    if (!animationId)
+      throw new Error(
+        "Animation trigger need `data-animate-trigger` attribute!"
+      );
+    const targetElement = document.querySelector(
+      `[data-animate-target="${animationId}"]`
+    );
+    if (!targetElement)
+      throw new Error(
+        "Animation target need same `data-animate-target` attribute as on trigger!"
+      );
+    this.targetElement = targetElement;
   },
 };
